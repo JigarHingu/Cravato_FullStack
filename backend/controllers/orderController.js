403a9b8a6df04c38cd3ad6d9deea4941f24bf559
Jigar Vijaybhai Hingu <165986885+JigarHingu@users.noreply.github.com>
2025-07-16 09:02:01 +0530

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -112,7 +112,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 // Place a new order
 const placeOrder = async (req, res) => {
-  const frontend_url = "http://localhost:5173";
+  const frontend_url = "https://cravato-frontend.onrender.com";
 
   try {
     const newOrder = new orderModel({
